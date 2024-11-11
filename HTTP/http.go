26e4f2557e61c80349ad26a472a459b3588a5770
Alberto Benatti <albi.benni8@gmail.com>
2024-11-11 10:04:53 +0100

    http get exmplained

--- HTTP/http.go
+++ HTTP/http.go
@@ -6,6 +6,13 @@ import (
 	"net/http"
 )
 
+// getIssueData fetches issue data from the specified API endpoint.
+// It sends a GET request to the API and returns the response body as a byte slice.
+// If an error occurs during the request or reading the response body, it returns the error.
+//
+// Returns:
+//   - []byte: The response body from the API.
+//   - error: An error if one occurred during the request or reading the response body.
 func getIssueData() ([]byte, error) {
 	res, err := http.Get("https://api.boot.dev/v1/courses_rest_api/learn-http/issues")
 	if err != nil {
@@ -17,6 +24,10 @@ func getIssueData() ([]byte, error) {
 	return data, err
 }
 
+// - http.Get uses the http.DefaultClient to make a request to the given url
+// - res is the HTTP response that comes back from the server
+// - defer res.Body.Close() ensures that the response body is properly closed after reading. Not doing so can cause memory issues.
+// - io.ReadAll reads the response body into a slice of bytes []byte called data
 func getProjects() ([]byte, error) {
 	res, err := http.Get("https://api.jello.com/projects")
 	if err != nil {

5ccc2bca81d230ae47dc8d08ba886134b42aa381
Alberto Benatti <albi.benni8@gmail.com>
2024-11-19 09:43:15 +0100

    unmarshal on web addres

--- HTTP/json-unmarshal.go
+++ HTTP/json-unmarshal.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-func getIssues(url string) ([]Issue, error) {
+func getIssues2(url string) ([]Issue, error) {
 	res, err := http.Get(url)
 	if err != nil {
 		return nil, fmt.Errorf("error creating request: %w", err)

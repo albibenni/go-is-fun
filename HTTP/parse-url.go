812aa90949737a3e893c08d060a7d2f3119729fc
Alberto Benatti <albi.benni8@gmail.com>
2024-11-23 14:44:53 +0100

    parse url

---
+++ HTTP/parse-url.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/url"
+)
+
+type ParsedURL struct {
+	protocol string
+	username string
+	password string
+	hostname string
+	port     string
+	pathname string
+	search   string
+	hash     string
+}
+
+func newParsedURL(urlString string) ParsedURL {
+	parsedUrl, err := url.Parse(urlString)
+	if err != nil {
+		return ParsedURL{}
+	}
+
+	password := ""
+	if pw, hasPassword := parsedUrl.User.Password(); hasPassword {
+		password = pw
+	}
+
+	return ParsedURL{
+		protocol: parsedUrl.Scheme,
+		username: parsedUrl.User.Username(),
+		password: password,
+		hostname: parsedUrl.Hostname(),
+		port:     parsedUrl.Port(),
+		pathname: parsedUrl.Path,
+		search:   parsedUrl.RawQuery,
+		hash:     parsedUrl.Fragment,
+	}
+}

4d566a1b87c993a2a00c54fbaf7c4bd95f8f11d3
Alberto Benatti <albi.benni8@gmail.com>
2025-04-12 14:14:47 +0200

    multiple return

--- pointers-and-ref/main.go
+++ pointers-and-ref/main.go
@@ -9,4 +9,7 @@ func main() {
 	var value float32 = 10.1
 	r1, r2 := practice(&value)
 	fmt.Printf("hello %v, %v \n", r1, r2)
+
+	s1, s2, s3 := multiple()
+	fmt.Printf("multiple return %v, %v, %v \n", s1, s2, s3)
 }
--- pointers-and-ref/yaya.go
+++ pointers-and-ref/yaya.go
@@ -4,3 +4,8 @@ func practice(num *float32) (float32, float32) {
 
 	return *num * 2, *num * 3
 }
+
+func multiple() (int, int, int) {
+
+	return 1, 2, 3
+}

bc063c9e7c908f4fe0311770ad231f880c8dec7e
Alberto Benatti <albi.benni8@gmail.com>
2025-04-13 15:54:51 +0200

    fun stuf

---
+++ types/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("----TYPESSS----\n")
+    printThem()
+	fmt.Printf("----TYPESSS----\n")
+}
---
+++ types/overloadPrint.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type Course struct {
+	Name string
+	Type string
+	Cost int
+}
+
+func (c Course) String() string {
+	return fmt.Sprintf("Name: %s, Type: %s, Cost: %d \n", c.Name, c.Type, c.Cost)
+}
+
+type Workshop struct {
+	Course
+}
+
+type WorkshopO struct {
+	Course Course
+}
+
+func printThem() {
+	//var wPrintable Workshop
+	wNonPrintable := WorkshopO{
+		Course: Course{
+			Name: "test",
+			Type: "my type",
+			Cost: 10,
+		},
+	}
+	wPrintable := Workshop{
+		Course: Course{
+			Name: "test",
+			Type: "my type",
+			Cost: 10,
+		},
+	}
+	fmt.Printf("%v \n", wNonPrintable)
+	fmt.Printf("%v \n", wPrintable)
+}

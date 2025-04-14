08423a496a9b2f03edec16e748bd89bd7b0f8203
Alberto Benatti <albi.benni8@gmail.com>
2025-04-14 19:49:01 +0200

    channels doc

--- goroutines/channels.go
+++ goroutines/channels.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+// Buff sends two test messages to the provided channel and then closes it.
+// This function is typically used to demonstrate channel communication in Go.
+//
+// Parameters:
+//   - channel: A string channel to which messages will be sent
+//
+// The function sends "test" and "test2" messages to the channel and then closes it
+// to signal that no more messages will be sent.
 func Buff(channel chan string) {
 	fmt.Println("Inside Buff")
 	channel <- "test"

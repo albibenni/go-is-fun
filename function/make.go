62ae275474af36dfc3e245050a1f3f58b52b6993
Alberto Benatti <albi.benni8@gmail.com>
2025-08-19 16:20:39 +0200

    slices

--- function/make.go
+++ function/make.go
@@ -14,4 +14,18 @@ func main() {
 	fmt.Printf("Creates slice with length now of %d with append, capacity -func cap() %d: %v\n", len(s2), cap(s2), s2)
 	fmt.Printf("Empty slice with capacity 5: %v\n", s3)
 	fmt.Printf("Empty slice with capacity 5: %v\n", s3)
+
+	// make([]type, length, capacity)
+	newSlice := make([]int, 3, 5) // length=3, capacity=5
+	fmt.Println(len(newSlice))    // prints: 3
+	fmt.Println(cap(newSlice))    // prints: 5
+	fmt.Println(newSlice)         // prints: [0 0 0]
+
+	// You can only access indices 0, 1, 2 initially
+	// newSlice[3] would cause a panic, even though capacity allows it
+
+	// But you can append without reallocating until you exceed capacity
+	newSlice = append(newSlice, 4) // now length=4, capacity still=5
+	newSlice = append(newSlice, 5) // now length=5, capacity=5
+	newSlice = append(newSlice, 6) // now length=6, capacity grows (typically doubles)
 }

907730030ccd5c6e645cb7a8d37c9793c3ba43a7
Alberto Benatti <albi.benni8@gmail.com>
2025-08-23 12:08:59 +0200

    a

--- dsa/array-and-slices.go
+++ dsa/array-and-slices.go
@@ -6,8 +6,9 @@ func main() {
 	s := make([]int, 8)
 	ar := [...]int{1, 2, 3, 4} // infer
 
+	fmt.Printf("type of slice s: %T, len: %d, cap: %d\n", s, len(s), cap(s))
 	s = append(s, 10)
 
-	fmt.Printf("type of slice s: %T, len: %d\n", s, len(s))
+	fmt.Printf("type of slice s: %T, len: %d, cap: %d\n", s, len(s), cap(s))
 	fmt.Printf("type of array ar: %T\n", ar)
 }

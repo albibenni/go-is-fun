9125af7bddb063cd9714bb96d83e6302dff05101
Alberto Benatti <albi.benni8@gmail.com>
2025-08-23 12:10:16 +0200

    dim runtime

--- dsa/array-and-slices.go
+++ dsa/array-and-slices.go
@@ -1,14 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
 	s := make([]int, 8)
 	ar := [...]int{1, 2, 3, 4} // infer
 
+	fmt.Printf("size of slice %d\n", unsafe.Sizeof(s))
 	fmt.Printf("type of slice s: %T, len: %d, cap: %d\n", s, len(s), cap(s))
 	s = append(s, 10)
 
+	fmt.Printf("size of slice %d\n", unsafe.Sizeof(s))
 	fmt.Printf("type of slice s: %T, len: %d, cap: %d\n", s, len(s), cap(s))
 	fmt.Printf("type of array ar: %T\n", ar)
 }

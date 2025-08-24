b44146a117d0585ebae21f13b7afb449137b7457
Alberto Benatti <albi.benni8@gmail.com>
2025-08-24 13:32:44 +0200

    var operator

--- function/variadic.go
+++ function/variadic.go
@@ -6,12 +6,14 @@ func main() {
 	fmt.Printf("------\n")
 
 	fmt.Printf("sum - %d\n", sum(1, 2, 5, 10))
-}
 
+	myNums := []int{1, 2, 51, 2, 2}
+	fmt.Printf("sum variadic op. - %d\n", sum(myNums...))
+}
 func sum(numbers ...int) (total int) {
 	total = 0
 	for _, num := range numbers {
 		total += num
 	}
-	return
+	return total
 }

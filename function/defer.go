dd49aca2b74e0020d8ee79d126d8d30f0078f711
Alberto Benatti <albi.benni8@gmail.com>
2025-08-26 19:05:18 +0200

    arguments are evaluated at the defer keyword moment

--- function/defer.go
+++ function/defer.go
@@ -4,7 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Starting main function")
+	fmt.Println("-------")
 	multipleDefers()
+	fmt.Println("-------")
+
+	multipleDefersFor()
 	fmt.Println("Ending main function")
 }
 func multipleDefers() {
@@ -17,3 +21,8 @@ func multipleDefers() {
 // Third defer
 // Second defer
 // First defer
+func multipleDefersFor() {
+	for i := 0; i < 10; i++ {
+		defer fmt.Printf("defer %d\n", i)
+	}
+}

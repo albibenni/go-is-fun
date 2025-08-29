157b91a2673c2e15e21ffa1372bd5351d7ae61ca
Alberto Benatti <albi.benni8@gmail.com>
2025-08-29 11:23:48 +0200

    testing generics with custom interface

--- generics/testing.go
+++ generics/testing.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
-
-	"golang.org/x/exp/constraints"
 )
 
-func max[T constraints.Ordered](a, b T) T {
+func max[T cmp.Ordered](a, b T) T {
 	if a >= b {
 		return a
 	}
 	return b
 }
 
+func Print[T any](s T) {
+	fmt.Println(s)
+}
+
 func main() {
 	fmt.Printf("Generics --- max 10 - 4: %d\n", max(10, 4))
+	fmt.Printf("Generics --- max A - a: %s\n", max("A", "a"))
+	Print("test generic print")
+	Print(10)
+	Print[int](10)
 }

b718588804d23dad352948ff24fab9fcd74434ae
Alberto Benatti <albi.benni8@gmail.com>
2025-08-29 11:48:15 +0200

    env

--- server/main.go
+++ server/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	fmt.Printf("Listening on port %s\n", port)
+
 }

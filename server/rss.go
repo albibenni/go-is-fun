b9bd436b81ad96ab4d73ab948f990f9e658b7668
Alberto Benatti <albi.benni8@gmail.com>
2025-09-04 21:03:27 +0200

    redo

---
+++ server/rss.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/xml"
+	"io"
+	"net/http"
+	"time"
+)
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Language    string    `xml:"language"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+// example URL: https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml
+// UrlToFeed fetches the RSS feed from the given URL and parses it into an RSSFeed struct.
+func urlToFeed(feedURL string) (RSSFeed, error) {
+	httpClient := http.Client{
+		Timeout: 10 * time.Second,
+	}
+	resp, err := httpClient.Get(feedURL)
+	if err != nil {
+		return RSSFeed{}, err
+	}
+	defer resp.Body.Close()
+
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return RSSFeed{}, err
+	}
+
+	var rssFeed RSSFeed
+	err = xml.Unmarshal(dat, &rssFeed)
+	if err != nil {
+		return RSSFeed{}, err
+	}
+
+	return rssFeed, nil
+}

f107219449e317c9afa800772b690b82dcdb238b
Alberto Benatti <albi.benni8@gmail.com>
2025-09-09 17:28:16 +0200

    complete

--- auth/main.go
+++ auth/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
-	"os"
 
+	"github.com/albibenni/go-exercises/auth/config"
+	"github.com/albibenni/go-exercises/auth/helpers"
 	"github.com/albibenni/go-exercises/auth/routes"
 	"github.com/albibenni/go-exercises/auth/types"
 	"github.com/gin-gonic/gin"
@@ -13,13 +15,24 @@ import (
 func main() {
 	godotenv.Load("local.env")
 
-	Key:= GenerateRandomKey()
-	SetJWTKey(key)
+	// Initialize database connection
+	db, err := config.InitDB()
+	if err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+	defer func() {
+		if err := db.Close(context.Background()); err != nil {
+			log.Printf("Error closing database: %v", err)
+		}
+	}()
+
+	jwtSecret := types.JWTSecret.Value()
+	helpers.SetJWTKey(jwtSecret)
 
 	r := gin.Default()
-	routes.SetupRoutes(r)
+	routes.SetupRoutes(r, db)
 
-	port := os.Getenv(types.ServerPath.Value())
+	port := types.ServerPort.Value()
 
 	r.Run(":" + port)
 	log.Println("Server is running on port: ", port)

6de095d5242f0f7a1b4bb7b8468a99b2b01216ad
Alberto Benatti <albi.benni8@gmail.com>
2025-09-09 17:27:40 +0200

    routes

--- auth/routes/routes.go
+++ auth/routes/routes.go
@@ -1,20 +1,21 @@
 package routes
 
 import (
+	"github.com/albibenni/go-exercises/auth/config"
+	"github.com/albibenni/go-exercises/auth/controller"
 	"github.com/albibenni/go-exercises/auth/middleware"
-	"github.com/albibenni/go-exercises/auth/services"
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRoutes(router *gin.Engine) {
-	router.POST("/signup", services.Signup())
-	router.POST("/login", services.Login())
+func SetupRoutes(router *gin.Engine, db *config.DB) {
+	router.POST("/signup", controller.Signup(db))
+	router.POST("/login", controller.Login(db))
 
 	protected := router.Group("/")
 	protected.Use(middleware.Authenticate())
 
 	{
-		protected.GET("/users", services.Getusers())
-		protected.GET("/user/:id", services.Getuser())
+		protected.GET("/users", controller.GetUsers(db))
+		protected.GET("/user/:id", controller.GetUser(db))
 	}
 }
